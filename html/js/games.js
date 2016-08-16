e5f8929c1cbbbf246e49518f565ebd00d5d76ce8
Alex Bezuska <abezuska@gmail.com>
2016-08-16 15:50:09 -0400

    update games made with splat

---
+++ html/js/games.js
@@ -0,0 +1,221 @@
+var games =  [
+	{
+		"play_url": "https://saidyeti.itch.io/marty-mcpaper-epic-delivery-service",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/saidyeti_martymcpaper.png",
+		"name": "Marty McPaper: Epic Delivery Service",
+		"developers": "Said Yeti"
+	}, {
+		"play_url": "https://hydrachickengames.itch.io/hydra-chicken",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/hydrachickengames_hyrdachicken.png",
+		"name": "Hydra Chicken",
+		"developers": "Hydra Chicken Games"
+	}, {
+		"play_url": "https://riseshinegames.itch.io/cali-bunga",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/riseandshinegames_calibunga.png",
+		"name": "Cali Bunga",
+		"developers": "Rise and Shine Games"
+	}, {
+		"play_url": "https://riseshinegames.itch.io/flip-flap-pong",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/riseandshinegames_flipflappong.png",
+		"name": "Flip Flap Pong",
+		"developers": "Rise and Shine Games"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/Electropolis",
+		"image": "img/games/twoscoopgames_electropolis.png",
+		"name": "Electropolis",
+		"developers": "Cara Smith, Alex Bezuska, Eric Lathrop, Rex Soriano, Lindar Greenwood"
+	}, {
+		"play_url": "",
+		"license": "MIT",
+		"github": "https://github.com/zbouvier/SplattyBird",
+		"image": "img/games/twoscoopgames_splattybird.png",
+		"name": "Splatty Bird",
+		"developers": "Zach Bouvier"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/Morning-Ritual",
+		"image": "img/games/twoscoopgames_morningritual.png",
+		"developers": "Two Scoop Games",
+		"name": "Morning Ritual"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/aquisenberry/ggj_meditate",
+		"image": "img/games/riseandshinegames_zenmadness.png",
+		"developers": "Rise ane Shine Games",
+		"name": "Zen Madness"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/riseandshinegames_polymorphic.png",
+		"developers": "Rise and Shine Games",
+		"name": "Polymorphic"
+	},
+	{
+		"play_url": "http://globalgamejam.org/2016/games/powers-bee",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/thepowersthatbee.png",
+		"developers": "@AustinCantRead, @ConnanBell, @LoLo_R, @senabryer",
+		"name": "The Powers That Bee"
+	},
+	{
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/Cluster-Junk",
+		"image": "img/games/twoscoopgames_clusterjunk.png",
+		"developers": "Two Scoop Games",
+		"name": "Cluster Junk"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/aquisenberry/DrunkenBossFight",
+		"image": "img/games/riseandshinegames_drunkenbossfight.png",
+		"developers": "Rise and Shine Games",
+		"name": "Drunken Boss Fight"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/Treatment-and-Control",
+		"image": "img/games/twoscoopgames_treatmentandcontrol.png",
+		"developers": "Control by Two Scoop Games",
+		"name": "Treatment and Control"
+	}, {
+		"play_url": "http://ludumdare.com/compo/ludum-dare-33/?action=preview&uid=35711",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/youarethemonster.png",
+		"developers": "Clay Morten, Frank Moussette, Rex Soriano, Tiffani Stuart",
+		"name": "You Are The Monster"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/twoscoopgames_stanleysqueaks.png",
+		"developers": "Two Scoop Games",
+		"name": "Stanley Squeaks and the Emerald Burrito"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/twoscoopgames_uprooted.png",
+		"developers": "Two Scoop Games",
+		"name": "Uprooted"
+	},
+
+	{
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/ggj15",
+		"image": "img/games/twoscoopgames_thedaytheworldchanged.png",
+		"developers": "Two Scoop Games",
+		"name": "The Day The World Changed"
+	},
+
+
+	{
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/twoscoopgames_desserts.png",
+		"developers": "Two Scoop Games",
+		"name": "Desserts Killed Your Daddy"
+	},
+
+	{
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/twoscoopgames_syrush.png",
+		"developers": "Two Scoop Games",
+		"name": "SyRush"
+	}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/twoscoopgames_mrfluffykins.png",
+		"developers": "Two Scoop Games",
+		"name": "Mr. Fluffykins' Great Sorting Adventure"
+	},
+
+	{
+		"play_url": "http://mintchipleaf.com/games/ludum/",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/mintgames_arkeynoid.png",
+		"developers": "Mint games",
+		"name": "Arkeynoid"
+	},
+
+
+ {
+		"play_url": "http://gamejolt.com/games/glazing-lazers/64688#close",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/alexbezuska_glazinglazers.png",
+		"developers": "Alex Bezuska",
+		"name": "Glazing Lazers"
+	},
+
+	{
+		"play_url": "http://globalgamejam.org/2015/games/go-man",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/goman.png",
+		"developers": "Anthony Quisenberry,Joey J Andrews,Jonathan Frazier,Rex Soriano,Wes Wright",
+		"name": "GO MAN!"
+	},
+
+	{
+		"play_url": "http://ludumdare.com/compo/ludum-dare-31/?action=preview&uid=3676",
+		"license": "Source-viewable proprietary",
+		"github": "",
+		"image": "img/games/foodfight.png",
+		"developers": "Clay Morton, Anthony Quisenberry, Kira Winters",
+		"name": "Food Fight",
+		"last_commit": "Apr 22, 2015"
+	},
+	{
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "http://mintchipleaf.com/games/basejump/",
+		"image": "img/games/mintgames_basejump.png",
+		"developers": "Mint games",
+		"name": "base.jump"
+	},{
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/kickbot",
+		"image": "img/games/twoscoopgames_kickbot.png",
+		"developers": "Two Scoop Games",
+		"name": "Kickbot"
+	},
+		{
+			"play_url": "",
+			"license": "Source-viewable proprietary",
+			"github": "",
+			"image": "img/games/twoscoopgames_apartment213.png",
+			"developers": "Two Scoop Games",
+			"name": "Apartment 213"
+		}, {
+		"play_url": "",
+		"license": "Source-viewable proprietary",
+		"github": "https://github.com/TwoScoopGames/scurry",
+		"image": "img/games/twoscoopgames_scurry.png",
+		"developers": "Two Scoop Games",
+		"name": "Scurry"
+	}
+
+];
---
+++ html/js/main.js
@@ -0,0 +1,66 @@
+// Github API stuff
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange=function(){
+	if(xhr.readyState==4 && xhr.status==200){
+		var contributors = JSON.parse(xhr.responseText);
+		var content = "";
+		for (var i = 0; i < contributors.length; i++){
+			var contributions = ' contributions';
+			if (contributors[i].contributions === 1){
+				contributions = ' contribution';
+			}
+			content += '<a class="person" href="http://github.com/' + contributors[i].login + '"><img src="' + contributors[i].avatar_url + '" class="image-responsive" /><div class="info">@'+ contributors[i].login +'<br/>' + contributors[i].contributions + contributions +'</div></a>';
+		}
+		var contrib = document.getElementById("contrib");
+		contrib.innerHTML = content;
+	}
+}
+xhr.open("GET","https://api.github.com/repositories/46762138/contributors",true);
+xhr.setRequestHeader("Accept","application/json");
+xhr.setRequestHeader("Origin","http://localhost:3000");
+
+xhr.send();
+
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange=function(){
+	if(xhr2.readyState==4 && xhr2.status==200){
+		var commits = JSON.parse(xhr2.responseText);
+		var commitMessage = document.getElementById("commitMessage");
+		commitMessage.innerHTML =
+		'Updated '
+		+ ' '
+		+ moment(commits[0].commit.committer.date).format('ll')
+		+ ' &mdash; '
+		+ commits[0].commit.message;
+
+	}
+}
+xhr2.open("GET","https://api.github.com/repositories/46762138/commits",true);
+xhr2.setRequestHeader("Accept","application/json");
+xhr2.setRequestHeader("Origin","http://localhost:3000");
+
+xhr2.send();
+
+
+function getLastCommitDate(repoID){
+	var xhr2 = new XMLHttpRequest();
+	xhr2.onreadystatechange=function(){
+		if(xhr2.readyState==4 && xhr2.status==200){
+			console.log(data);
+			var data = JSON.parse(xhr2.responseText);
+			return  moment(data.updated_at).format('ll');
+		}
+	}
+	xhr2.open("GET",repoID,true);
+	xhr2.setRequestHeader("Accept","application/json");
+	xhr2.setRequestHeader("Origin","http://localhost:3000");
+
+	xhr2.send();
+}
+
+
+console.log(getLastCommitDate("https://api.github.com/TwoScoopGames/Cluster-Junk"));
+
+// Handlebars stuff
+var gameTemplate = Handlebars.compile(document.getElementById('game-template').innerHTML);
+document.getElementById('games').innerHTML = gameTemplate(games);
