89939a586a9147082c80065acab3a4e273f2afe7
Alex Bezuska <abezuska@gmail.com>
2016-08-22 22:15:16 -0400

    update games list

--- html/js/games.js
+++ html/js/games.js
@@ -1,221 +1,356 @@
-var games =  [
-	{
+var games =  [{
 		"play_url": "https://saidyeti.itch.io/marty-mcpaper-epic-delivery-service",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/saidyeti_martymcpaper.png",
 		"name": "Marty McPaper: Epic Delivery Service",
-		"developers": "Said Yeti"
-	}, {
-		"play_url": "https://hydrachickengames.itch.io/hydra-chicken",
-		"license": "Source-viewable proprietary",
-		"github": "",
-		"image": "img/games/hydrachickengames_hyrdachicken.png",
+		"teamMembers": "Said Yeti"
+	},
+
+
+
+	{
 		"name": "Hydra Chicken",
-		"developers": "Hydra Chicken Games"
-	}, {
+		"play_url": "http://rex-soriano.com/LudumDare34/HHHC/",
+		"github": "https://github.com/awesomerex/HungryHungryHydraChicken",
+		"license": "Source-viewable proprietary",
+		"image": "img/games/hydrachickengames_hyrdachicken.png",
+		"teamMembers": "Hydra Chicken Games",
+		"splat": "",
+		"lastchange": ""
+	},
+
+
+
+
+
+	{
 		"play_url": "https://riseshinegames.itch.io/cali-bunga",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/riseandshinegames_calibunga.png",
 		"name": "Cali Bunga",
-		"developers": "Rise and Shine Games"
-	}, {
+		"teamMembers": "Rise and Shine Games"
+	},
+
+
+
+	{
 		"play_url": "https://riseshinegames.itch.io/flip-flap-pong",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/riseandshinegames_flipflappong.png",
 		"name": "Flip Flap Pong",
-		"developers": "Rise and Shine Games"
-	}, {
+		"teamMembers": "Rise and Shine Games"
+	},
+
+
+
+	{
 		"play_url": "",
 		"license": "Source-viewable proprietary",
 		"github": "https://github.com/TwoScoopGames/Electropolis",
 		"image": "img/games/twoscoopgames_electropolis.png",
 		"name": "Electropolis",
-		"developers": "Cara Smith, Alex Bezuska, Eric Lathrop, Rex Soriano, Lindar Greenwood"
-	}, {
-		"play_url": "",
-		"license": "MIT",
-		"github": "https://github.com/zbouvier/SplattyBird",
-		"image": "img/games/twoscoopgames_splattybird.png",
+		"teamMembers": "Cara Smith, Alex Bezuska, Eric Lathrop, Rex Soriano, Lindar Greenwood"
+	},
+
+
+
+	{
 		"name": "Splatty Bird",
-		"developers": "Zach Bouvier"
-	}, {
-		"play_url": "",
-		"license": "Source-viewable proprietary",
+		"play_url": "http://twoscoopgames.com/splattybird/",
+		"github": "https://github.com/zbouvier/SplattyBird",
+		"license": "MIT",
+		"image": "img/games/twoscoopgames_splattybird.png",
+		"teamMembers": "Zach Bouvier"
+	},
+
+
+
+	{
+		"name": "Morning Ritual",
+		"play_url": "http://twoscoopgames.com/morningritual/game/",
 		"github": "https://github.com/TwoScoopGames/Morning-Ritual",
-		"image": "img/games/twoscoopgames_morningritual.png",
-		"developers": "Two Scoop Games",
-		"name": "Morning Ritual"
-	}, {
-		"play_url": "",
 		"license": "Source-viewable proprietary",
+		"image": "img/games/twoscoopgames_morningritual.png",
+		"teamMembers": "Two Scoop Games, Cara Smith, Chris Kincaid",
+		"descripion": "Every morning we go through a delicate morning ritual to prepare ourselves for the onslaught of the day."
+	},
+
+
+
+	{
+		"name": "Zen Madness",
+		"play_url": "http://aquisenberry.github.io/ggj_meditate/build/",
 		"github": "https://github.com/aquisenberry/ggj_meditate",
+		"license": "Source-viewable proprietary",
 		"image": "img/games/riseandshinegames_zenmadness.png",
-		"developers": "Rise ane Shine Games",
-		"name": "Zen Madness"
-	}, {
+		"teamMembers": "Rise ane Shine Games",
+		"descripion": ""
+	},
+
+
+
+	{
 		"play_url": "",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/riseandshinegames_polymorphic.png",
-		"developers": "Rise and Shine Games",
+		"teamMembers": "Rise and Shine Games",
 		"name": "Polymorphic"
 	},
+
+
+
 	{
-		"play_url": "http://globalgamejam.org/2016/games/powers-bee",
+		"name": "The Powers That Bee",
+		"play_url": "http://austinread.github.io/thepowersthatbee/",
+		"github": "https://github.com/awesomerex/thepowersthatbee",
 		"license": "Source-viewable proprietary",
-		"github": "",
 		"image": "img/games/thepowersthatbee.png",
-		"developers": "@AustinCantRead, @ConnanBell, @LoLo_R, @senabryer",
-		"name": "The Powers That Bee"
+		"teamMembers": "@AustinCantRead, @ConnanBell, @LoLo_R, @senabryer",
+		"descripion": "The Queen bee is an idiot and sold all of the precious artifacts required to summon the Spring Goddess and end winter. Use your worker and warrior bees to gather the artifacts and bring about Spring! Hints: Warrior bees are required to kill monsters. Worker bees are required to bring artifacts back to the hive. Controls: WASD to move. Left-click on the beehive to summon a worker bee, right-click to summon a warrior bees. Away from the hive clicking releases a bee.",
+		"splat": "",
+		"lastchange": "Jan 31, 2016"
 	},
+
+
+
 	{
-		"play_url": "",
-		"license": "Source-viewable proprietary",
+		"name": "Cluster Junk",
+		"play_url": "http://twoscoopgames.com/ld34/",
 		"github": "https://github.com/TwoScoopGames/Cluster-Junk",
+		"license": "Source-viewable proprietary",
 		"image": "img/games/twoscoopgames_clusterjunk.png",
-		"developers": "Two Scoop Games",
-		"name": "Cluster Junk"
-	}, {
-		"play_url": "",
-		"license": "Source-viewable proprietary",
+		"teamMembers": "Two Scoop Games, Cara Smith, Ben Wiley",
+		"descripion": ""
+	},
+
+
+
+	{
+		"name": "Drunken Boss Fight",
+		"play_url": "https://github.com/aquisenberry/DrunkenBossFight",
 		"github": "https://github.com/aquisenberry/DrunkenBossFight",
+		"license": "Source-viewable proprietary",
 		"image": "img/games/riseandshinegames_drunkenbossfight.png",
-		"developers": "Rise and Shine Games",
-		"name": "Drunken Boss Fight"
-	}, {
-		"play_url": "",
-		"license": "Source-viewable proprietary",
+		"teamMembers": "Rise and Shine Games",
+		"descripion": ""
+	},
+
+
+
+
+	{
+		"name": "Treatment and Control",
+		"play_url": "http://twoscoopgames.com/ludumdare33/",
 		"github": "https://github.com/TwoScoopGames/Treatment-and-Control",
+		"license": "Source-viewable proprietary",
 		"image": "img/games/twoscoopgames_treatmentandcontrol.png",
-		"developers": "Control by Two Scoop Games",
-		"name": "Treatment and Control"
-	}, {
-		"play_url": "http://ludumdare.com/compo/ludum-dare-33/?action=preview&uid=35711",
+		"teamMembers": "Two Scoop Games, Chris Kincaid",
+		"descripion": ""
+	},
+
+
+
+	{
+		"name": "You Are The Monster",
+		"play_url": "http://rex-soriano.com/ld33/",
+		"github": "https://github.com/awesomerex/LudumDare33/tree/test_city",
+		"teamMembers": "Clay Morten, Rex Soriano, Frank Moussette, Tiffani Stuart",
 		"license": "Source-viewable proprietary",
-		"github": "",
 		"image": "img/games/youarethemonster.png",
-		"developers": "Clay Morten, Frank Moussette, Rex Soriano, Tiffani Stuart",
-		"name": "You Are The Monster"
-	}, {
-		"play_url": "",
+		"descripion": "You wake up one day and realize that you are a giant old man. Fed up with your reality you decide to go on a rampage, Tearing down the city that allowed it all to come to this. ",
+		"splat": "3.0.0",
+		"lastchange": "Aug 23, 2015"
+	},
+
+
+
+	{
+		"play_url": "http://twoscoopgames.com/stanleysqueaks/game/",
+		"github": "https://github.com/TwoScoopGames/StanleySqueaks",
 		"license": "Source-viewable proprietary",
-		"github": "",
 		"image": "img/games/twoscoopgames_stanleysqueaks.png",
-		"developers": "Two Scoop Games",
-		"name": "Stanley Squeaks and the Emerald Burrito"
-	}, {
-		"play_url": "",
+		"teamMembers": "Two Scoop Games",
+		"name": "Stanley Squeaks and the Emerald Burrito",
+		"descripion": "Stanley Squeaks is an adventuring hamster on a quest to obtain the Emerald Burrito. Help Stanley solve puzzles and avoid traps in an ancient temple. This game is a puzzle platformer where the blocks you touch on the way to the treasure are destroyed on the way out, making it difficult to get out alive!",
+		"splat": "3.1.0",
+		"lastchange": "Sep 17, 2015"
+	},
+
+
+
+	{
+		"name": "Uprooted",
+		"play_url": "http://twoscoopgames.com/ld32/",
+		"github": "https://github.com/TwoScoopGames/ld32",
 		"license": "Source-viewable proprietary",
-		"github": "",
 		"image": "img/games/twoscoopgames_uprooted.png",
-		"developers": "Two Scoop Games",
-		"name": "Uprooted"
+		"teamMembers": "Two Scoop Games",
+		"descripion": ""
 	},
 
+
+
 	{
-		"play_url": "",
-		"license": "Source-viewable proprietary",
+		"name": "The Day The World Changed",
+		"play_url": "http://twoscoopgames.com/ggj15/",
 		"github": "https://github.com/TwoScoopGames/ggj15",
+		"license": "Source-viewable proprietary",
 		"image": "img/games/twoscoopgames_thedaytheworldchanged.png",
-		"developers": "Two Scoop Games",
-		"name": "The Day The World Changed"
+		"teamMembers": "Two Scoop Games",
+		"descripion": ""
 	},
 
 
+
 	{
-		"play_url": "",
+		"name": "Desserts Killed Your Daddy",
+		"play_url": "http://twoscoopgames.com/ludumdare31/",
+		"github": "https://github.com/TwoScoopGames/Deserts-Killed-Your-Daddy",
 		"license": "Source-viewable proprietary",
-		"github": "",
 		"image": "img/games/twoscoopgames_desserts.png",
-		"developers": "Two Scoop Games",
-		"name": "Desserts Killed Your Daddy"
+		"teamMembers": "Two Scoop Games",
+		"descripion": "",
+		"splat": "3.0.2",
+		"lastchange": "Dec 8, 2014"
 	},
 
+
+
 	{
-		"play_url": "",
+		"name": "SyRush",
+		"play_url": "http://twoscoopgames.com/syrush/game/",
+		"github": "https://github.com/TwoScoopGames/SyRUSH",
 		"license": "Source-viewable proprietary",
-		"github": "",
 		"image": "img/games/twoscoopgames_syrush.png",
-		"developers": "Two Scoop Games",
-		"name": "SyRush"
-	}, {
-		"play_url": "",
-		"license": "Source-viewable proprietary",
-		"github": "",
-		"image": "img/games/twoscoopgames_mrfluffykins.png",
-		"developers": "Two Scoop Games",
-		"name": "Mr. Fluffykins' Great Sorting Adventure"
+		"teamMembers": "Two Scoop Games",
+		"descripion": "",
+		"splat": "",
+		"lastchange": "Sep 22, 2014"
 	},
+
+
+
+	{
+		"name": "Mr. Fluffykins' Great Sorting Adventure",
+		"play_url": "http://twoscoopgames.com/fluffykins/game/",
+		"github": "https://github.com/TwoScoopGames/Mr-Fluffykins-Great-Sorting-Adventure",
+		"license": "Source-viewable proprietary",
+		"image": "img/games/twoscoopgames_mrfluffykins.png",
+		"teamMembers": "Two Scoop Games, Anthony Quisenberry, Rex Soriano, Matthew Hutchens, Aaron Smith",
+		"descripion": "",
+		"splat": "3.0.2",
+		"lastchange": "Dec 8, 2014"
+	},
+
+
 
 	{
 		"play_url": "http://mintchipleaf.com/games/ludum/",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/mintgames_arkeynoid.png",
-		"developers": "Mint games",
+		"teamMembers": "Mint games",
 		"name": "Arkeynoid"
 	},
 
 
- {
+
+	{
 		"play_url": "http://gamejolt.com/games/glazing-lazers/64688#close",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/alexbezuska_glazinglazers.png",
-		"developers": "Alex Bezuska",
+		"teamMembers": "Alex Bezuska",
 		"name": "Glazing Lazers"
 	},
+
+
 
 	{
 		"play_url": "http://globalgamejam.org/2015/games/go-man",
 		"license": "Source-viewable proprietary",
 		"github": "",
 		"image": "img/games/goman.png",
-		"developers": "Anthony Quisenberry,Joey J Andrews,Jonathan Frazier,Rex Soriano,Wes Wright",
+		"teamMembers": "Anthony Quisenberry,Joey J Andrews,Jonathan Frazier,Rex Soriano,Wes Wright",
 		"name": "GO MAN!"
 	},
 
-	{
-		"play_url": "http://ludumdare.com/compo/ludum-dare-31/?action=preview&uid=3676",
-		"license": "Source-viewable proprietary",
-		"github": "",
-		"image": "img/games/foodfight.png",
-		"developers": "Clay Morton, Anthony Quisenberry, Kira Winters",
-		"name": "Food Fight",
-		"last_commit": "Apr 22, 2015"
-	},
-	{
-		"play_url": "",
-		"license": "Source-viewable proprietary",
-		"github": "http://mintchipleaf.com/games/basejump/",
-		"image": "img/games/mintgames_basejump.png",
-		"developers": "Mint games",
-		"name": "base.jump"
-	},{
-		"play_url": "",
-		"license": "Source-viewable proprietary",
-		"github": "https://github.com/TwoScoopGames/kickbot",
-		"image": "img/games/twoscoopgames_kickbot.png",
-		"developers": "Two Scoop Games",
-		"name": "Kickbot"
-	},
-		{
-			"play_url": "",
-			"license": "Source-viewable proprietary",
-			"github": "",
-			"image": "img/games/twoscoopgames_apartment213.png",
-			"developers": "Two Scoop Games",
-			"name": "Apartment 213"
-		}, {
-		"play_url": "",
-		"license": "Source-viewable proprietary",
-		"github": "https://github.com/TwoScoopGames/scurry",
-		"image": "img/games/twoscoopgames_scurry.png",
-		"developers": "Two Scoop Games",
-		"name": "Scurry"
-	}
 
+
+	{
+		"name": "Food Fight",
+		"play_url": "https://aquisenberry.itch.io/food-fight",
+		"github": "https://github.com/aquisenberry/FoodFight",
+		"license": "Source-viewable proprietary",
+		"image": "img/games/foodfight.png",
+		"teamMembers": "Clay Morton, Anthony Quisenberry, Kira Winters",
+		"descripion": "Entry for Ludum Dare 32. This is a Local multiplayer food fighter. This is a minimal product and we plan on working on this more and releasing it with tablet friendly controls. Please play and have fun :-)",
+		"splat": "",
+		"lastchange": "Apr 22, 2015"
+	},
+
+
+
+
+	{
+		"name": "base.jump",
+		"play_url": "http://mintchipleaf.com/games/basejump/",
+		"license": "proprietary",
+		"image": "img/games/mintgames_basejump.png",
+		"teamMembers": "Mint games"
+	},
+
+
+
+	{
+		"name": "Kick Bot",
+		"play_url": "http://twoscoopgames.com/kickbot/game/",
+		"github": "https://github.com/TwoScoopGames/kickbot",
+		"license": "Source-viewable proprietary",
+		"image": "img/games/twoscoopgames_kickbot.png",
+		"teamMembers": "Two Scoop Games",
+		"descripion": "",
+		"splat": "",
+		"lastchange": "Jul 21, 2014"
+	},
+
+
+
+	{
+		"name": "Apartment 213",
+		"play_url": "http://twoscoopgames.com/apartment213/game/",
+		"github": "https://github.com/TwoScoopGames/apt-213-the-struggle",
+		"license": "Source-viewable proprietary",
+		"image": "img/games/twoscoopgames_apartment213.png",
+		"teamMembers": "Two Scoop Games, Mike Revel, Mattie Richards",
+		"descripion": "",
+		"splat": "",
+		"lastchange": "Feb 2, 2014"
+	},
+
+
+
+	{
+		"name": "Scurry",
+		"slogan":"Run, jump, survive."
+		"play_url": "http://twoscoopgames.com/scurry/game",
+		"github": "https://github.com/TwoScoopGames/scurry",
+		"platforms":{
+			"web": "http://twoscoopgames.com/scurry/game",
+			"ios": "https://itunes.apple.com/us/app/scurry/id892274154?ls=1&mt=8",
+			"chromewebstore":"https://chrome.google.com/webstore/detail/scurry/djoifiholmpdceikfaajckcjepbjolof"
+		}
+		"license": "Source-viewable proprietary",
+		"image": "img/games/twoscoopgames_scurry.png",
+		"teamMembers": "Two Scoop Games",
+		"descripion": "",
+		"splat": "1.1.0",
+		"lastchange": "Jul 21, 2014",
+		"descripion"; "Scurry is a game where you play as a cockroach who is running through a grocery store, avoid obstacles, get sweet power-ups, and try not to fall! Try to get a high score and beat your friends!",
+		"webpage":"http://twoscoopgames.com/scurry/"
+	}
 ];
